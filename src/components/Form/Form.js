e99645525c8fc3ae6f95a5b901f53f1b5d3bd5e4
AvocadoCasualty <baibeebambi@gmail.com>
2020-05-31 17:33:17 -0700

    fixed the cancel button

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -9,6 +9,7 @@ class Form extends Component{
             price: 0,
             imgUrl: ''
         }
+        this.cancelInput = this.cancelInput.bind(this)
     }
     nameHandleChange(name) {
         this.setState({name});
@@ -19,30 +20,37 @@ class Form extends Component{
     imgUrlHandleChange(imgUrl) {
         this.setState({imgUrl})
     }
-    // cancelInput () {
-    //     this.state.reset();
-    // }
+    cancelInput () {
+        this.setState({
+            name: '',
+            price: 0,
+            imgUrl: ''
+        });
+    }
     render() {
         return (
             <div className='form-div'>
                 Name:
                 <br/>
                 <input
+                    value={this.state.name}
                     onChange={(e)=>this.nameHandleChange(e.target.value)}/>
                     <br/>
                     Price:
                 <br/>
                 <input
+                    value={this.state.price}
                     onChange={(e)=>this.priceHandleChange(e.target.value)}/>
                     <br/>
                     Image URL:
                 <br/>
                 <input
+                    value={this.state.imgUrl}
                     onChange={(e)=>this.imgUrlHandleChange(e.target.value)}/>
                     <br/>
-                {/*<button*/}
-                {/*    onClick={this.cancelInput}*/}
-                {/*    className='Cancel'>Cancel</button>*/}
+                <button
+                    onClick={this.cancelInput}
+                    className='Cancel'>Cancel</button>
                 <button className='Add-to-inventory'>Add to Inventory</button>
             </div>
         )
